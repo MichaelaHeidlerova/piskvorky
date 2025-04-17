64f3907bb0b159e57c95a43f8e7c520313a021d7
Michaela Heidlerová <michaela.heidlerova@gmail.com>
2025-04-17 13:58:54 +0200

    Interaktivní prvky hry (bez bonusu)

---
+++ hra.js
@@ -0,0 +1,27 @@
+let currentPlayer = 'circle';
+const currentPlayerInfoElement = document.getElementById('currentPlayerInfo'); // Používáš existující element
+
+const handleFieldClick = (event) => {
+  const clickedField = event.target;
+  if (currentPlayer === 'circle') {
+    clickedField.classList.add('black-circle');
+    currentPlayer = 'cross';
+    currentPlayerInfoElement.classList.remove('player-circle');
+    currentPlayerInfoElement.classList.add('player-cross');
+  } else {
+    clickedField.classList.add('black-cross');
+    currentPlayer = 'circle';
+    currentPlayerInfoElement.classList.remove('player-cross');
+    currentPlayerInfoElement.classList.add('player-circle');
+  }
+
+  clickedField.disabled = true;
+}
+
+const fields = document.querySelectorAll('.cell');
+
+fields.forEach(field => {
+  field.addEventListener('click', handleFieldClick);
+});
+
+currentPlayerInfoElement.classList.add('player-circle');
